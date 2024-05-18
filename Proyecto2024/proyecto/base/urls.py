a87f9c5617457fd6a84bdf90893e7a1e21edd6ee
carlos-alvarez-duoc <carlo.alvareza@duocuc.cl>
2024-05-18 01:56:39 -0400

    Commit feature
    Primer commit de feature_N1

--- Proyecto2024/proyecto/base/urls.py
+++ Proyecto2024/proyecto/base/urls.py
@@ -1,5 +1,6 @@
 """docstring"""
 from django.urls import path
-from .views import ListaPendientes
+from .views import ListaPendientes, DetalleTarea
 
-urlpatterns = [path('', ListaPendientes.as_view(), name='pendientes')]
+urlpatterns = [path('', ListaPendientes.as_view(), name='pendientes'),
+               path('tarea/<int:pk>', DetalleTarea.as_view(), name='tarea')]
