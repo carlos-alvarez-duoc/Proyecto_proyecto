75b4ea23ae7f998285e197145c27f71e66277a3e
carlos-alvarez-duoc <carlo.alvareza@duocuc.cl>
2024-05-18 02:20:03 -0400

    Agregar nueva tarea
    Decorado del html

--- Proyecto2024/proyecto/base/apps.py
+++ Proyecto2024/proyecto/base/apps.py
@@ -1,6 +1,8 @@
+"""docstring"""
 from django.apps import AppConfig
 
 
 class BaseConfig(AppConfig):
+    """docstring"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'base'
--- Proyecto2024/proyecto/base/views.py
+++ Proyecto2024/proyecto/base/views.py
@@ -2,6 +2,8 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .models import Tarea
 
 class ListaPendientes(ListView):
@@ -14,3 +16,10 @@ class DetalleTarea(DetailView):
     model = Tarea
     context_object_name = 'tarea'
     template_name = 'base/tarea.html'
+
+class CrearTarea(CreateView):
+    """docstring"""
+    model = Tarea
+    fields = '__all__'
+    success_url = reverse_lazy('tareas')
+
